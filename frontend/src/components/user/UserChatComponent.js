3a8f53110790ce52ed11580ed0e88f4b0f2619d9
Sorooj Shehryar <soroojshehryar@Macs-MacBook-Pro.local>
2024-01-03 00:08:12 +0500

    added some web pages

---
+++ frontend/src/components/user/UserChatComponent.js
@@ -0,0 +1,15 @@
+import "../../chats.css";
+const UserChatComponent = () => {
+  return (
+    <>
+      <input type="checkbox" id="check" />
+      <label className="chat-btn" htmlFor="check">
+        <i className="bi bi-chat-dots comment"></i>
+        <i className="bi bi-x-circle close"></i>
+      </label>
+    </>
+  );
+};
+
+export default UserChatComponent;
+
