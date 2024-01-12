b5720d13055bab2b0dac36b57b70ca07ea6e028c
Sorooj Shehryar <soroojshehryar@Macs-MacBook-Pro.local>
2024-01-13 01:14:16 +0500

    backup

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.use((req, res, next) => {
-    console.log("first middleware");
-    next()
-})
+const apiRoutes = require("./routes/apiRoutes");
 
-app.get('/', (req, res, next) => {
-    console.log("second middleware");
-  res.send('Hello World!')
-//   next()
-})
+app.get("/", async (req, res, next) => {
+  res.json({ message: "API running..." });
+});
 
-app.get('/two', (req, res) => {
-    console.log("third middleware");
-  res.send('Hello World 2!')
-})
+// mongodb connection
+const connectDB = require("./config/db");
+connectDB();
 
-app.use((req,res) => {
-    console.log("fourth middleware");
-})
+app.use("/api", apiRoutes);
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(500).json({
+    message: error.message,
+    stack: error.stack,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
