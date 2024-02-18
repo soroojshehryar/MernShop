62713830befcbfb8a9a50f4cb1277335c6575526
Sorooj Shehryar <soroojshehryar@192.168.1.7>
2024-02-18 20:49:02 +0500

    updated get products and added getProductbyId and getBestSellers to proucts controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,23 +4,54 @@ const recordsPerPage = require("../config/pagination");
 const getProducts = async (req, res, next) => {
   try {
     let query = {};
-    let queryCondition = false
+    let queryCondition = false;
 
     let priceQueryCondition = {};
     if (req.query.price) {
-        queryCondition = true;
+      queryCondition = true;
       priceQueryCondition = { price: { $lte: Number(req.query.price) } };
     }
     let ratingQueryCondition = {};
     if (req.query.rating) {
-        queryCondition = true;
+      queryCondition = true;
       ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
     }
-
-    if (queryCondition) {
-      query = {
-        $and: [priceQueryCondition, ratingQueryCondition],
+    let categoryQueryCondition = {};
+    const categoryName = req.params.categoryName || "";
+    if (categoryName) {
+      queryCondition = true;
+      let a = categoryName.replaceAll(",", "/");
+      var regEx = new RegExp("^" + a);
+      categoryQueryCondition = { category: regEx };
+    }
+    if (req.query.category) {
+      queryCondition = true;
+      let a = req.query.category.split(",").map((item) => {
+        if (item) return new RegExp("^" + item);
+      });
+      categoryQueryCondition = {
+        category: { $in: a },
       };
+    }
+    let attrsQueryCondition = [];
+    if (req.query.attrs) {
+      // attrs=RAM-1TB-2TB-4TB,color-blue-red
+      // [ 'RAM-1TB-4TB', 'color-blue', '' ]
+      attrsQueryCondition = req.query.attrs.split(",").reduce((acc, item) => {
+        if (item) {
+          let a = item.split("-");
+          let values = [...a];
+          values.shift(); // removes first item
+          let a1 = {
+            attrs: { $elemMatch: { key: a[0], value: { $in: values } } },
+          };
+          acc.push(a1);
+          // console.dir(acc, { depth: null })
+          return acc;
+        } else return acc;
+      }, []);
+    //   console.dir(attrsQueryCondition, { depth: null });
+    queryCondition = true;
     }
 
     //pagination
@@ -34,8 +65,33 @@ const getProducts = async (req, res, next) => {
       sort = { [sortOpt[0]]: Number(sortOpt[1]) };
     }
 
+    const searchQuery = req.params.searchQuery || ""
+    let searchQueryCondition = {}
+    let select = {}
+    if(searchQuery) {
+        queryCondition = true
+        searchQueryCondition = { $text: { $search: searchQuery } }
+        select = {
+            score: { $meta: "textScore" }
+        }
+        sort = { score: { $meta: "textScore" } }
+    }
+
+    if (queryCondition) {
+      query = {
+        $and: [
+          priceQueryCondition,
+          ratingQueryCondition,
+          categoryQueryCondition,
+          searchQueryCondition,
+          ...attrsQueryCondition
+        ],
+      };
+    }
+
     const totalProducts = await Product.countDocuments(query);
     const products = await Product.find(query)
+        .select(select)
       .skip(recordsPerPage * (pageNum - 1))
       .sort(sort)
       .limit(recordsPerPage);
@@ -49,5 +105,31 @@ const getProducts = async (req, res, next) => {
     next(error);
   }
 };
-module.exports = getProducts;
+
+const getProductById = async (req, res, next) => {
+    try {
+        const product = await Product.findById(req.params.id).populate("reviews").orFail()
+        res.json(product)
+    } catch(err) {
+        next(err)
+    }
+}
+
+const getBestsellers = async (req, res, next) => {
+    try {
+        const products = await Product.aggregate([
+            { $sort: { category: 1, sales: -1 } },
+            { $group: { _id: "$category", doc_with_max_sales: { $first: "$$ROOT" } } },
+            { $replaceWith: "$doc_with_max_sales" },
+            { $match: { sales: { $gt: 0 } } },
+            { $project: { _id: 1, name: 1, images: 1, category: 1, description: 1 } },
+            { $limit: 3 }
+        ])
+        res.json(products)
+    } catch(err) {
+        next(err)
+    }
+}
+
+module.exports = {getProducts, getProductById, getBestsellers};
 
