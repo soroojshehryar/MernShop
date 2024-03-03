76d83aec1924ebff18393c018a8070711e1b6eb1
Sorooj Shehryar <soroojshehryar@192.168.1.3>
2024-03-03 18:14:41 +0500

    Added image upload functionality along with user user creation method

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,6 @@
 const Product = require("../models/ProductModel");
 const recordsPerPage = require("../config/pagination");
+const imageValidate = require("../utils/imageValidate");
 
 const getProducts = async (req, res, next) => {
   try {
@@ -50,8 +51,8 @@ const getProducts = async (req, res, next) => {
           return acc;
         } else return acc;
       }, []);
-    //   console.dir(attrsQueryCondition, { depth: null });
-    queryCondition = true;
+      //   console.dir(attrsQueryCondition, { depth: null });
+      queryCondition = true;
     }
 
     //pagination
@@ -65,16 +66,16 @@ const getProducts = async (req, res, next) => {
       sort = { [sortOpt[0]]: Number(sortOpt[1]) };
     }
 
-    const searchQuery = req.params.searchQuery || ""
-    let searchQueryCondition = {}
-    let select = {}
-    if(searchQuery) {
-        queryCondition = true
-        searchQueryCondition = { $text: { $search: searchQuery } }
-        select = {
-            score: { $meta: "textScore" }
-        }
-        sort = { score: { $meta: "textScore" } }
+    const searchQuery = req.params.searchQuery || "";
+    let searchQueryCondition = {};
+    let select = {};
+    if (searchQuery) {
+      queryCondition = true;
+      searchQueryCondition = { $text: { $search: searchQuery } };
+      select = {
+        score: { $meta: "textScore" },
+      };
+      sort = { score: { $meta: "textScore" } };
     }
 
     if (queryCondition) {
@@ -84,14 +85,14 @@ const getProducts = async (req, res, next) => {
           ratingQueryCondition,
           categoryQueryCondition,
           searchQueryCondition,
-          ...attrsQueryCondition
+          ...attrsQueryCondition,
         ],
       };
     }
 
     const totalProducts = await Product.countDocuments(query);
     const products = await Product.find(query)
-        .select(select)
+      .select(select)
       .skip(recordsPerPage * (pageNum - 1))
       .sort(sort)
       .limit(recordsPerPage);
@@ -107,29 +108,186 @@ const getProducts = async (req, res, next) => {
 };
 
 const getProductById = async (req, res, next) => {
-    try {
-        const product = await Product.findById(req.params.id).populate("reviews").orFail()
-        res.json(product)
-    } catch(err) {
-        next(err)
-    }
-}
+  try {
+    const product = await Product.findById(req.params.id)
+      .populate("reviews")
+      .orFail();
+    res.json(product);
+  } catch (err) {
+    next(err);
+  }
+};
 
 const getBestsellers = async (req, res, next) => {
-    try {
-        const products = await Product.aggregate([
-            { $sort: { category: 1, sales: -1 } },
-            { $group: { _id: "$category", doc_with_max_sales: { $first: "$$ROOT" } } },
-            { $replaceWith: "$doc_with_max_sales" },
-            { $match: { sales: { $gt: 0 } } },
-            { $project: { _id: 1, name: 1, images: 1, category: 1, description: 1 } },
-            { $limit: 3 }
-        ])
-        res.json(products)
-    } catch(err) {
-        next(err)
-    }
-}
+  try {
+    const products = await Product.aggregate([
+      { $sort: { category: 1, sales: -1 } },
+      {
+        $group: { _id: "$category", doc_with_max_sales: { $first: "$$ROOT" } },
+      },
+      { $replaceWith: "$doc_with_max_sales" },
+      { $match: { sales: { $gt: 0 } } },
+      { $project: { _id: 1, name: 1, images: 1, category: 1, description: 1 } },
+      { $limit: 3 },
+    ]);
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+};
 
-module.exports = {getProducts, getProductById, getBestsellers};
+const adminGetProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find({})
+      .sort({ category: 1 })
+      .select("name price category");
+    return res.json(products);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const adminDeleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id).orFail();
+    await product.remove();
+    res.json({ message: "product removed" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const adminCreateProduct = async (req, res, next) => {
+  try {
+    const product = new Product();
+    const { name, description, count, price, category, attributesTable } =
+      req.body;
+    product.name = name;
+    product.description = description;
+    product.count = count;
+    product.price = price;
+    product.category = category;
+    if (attributesTable.length > 0) {
+      attributesTable.map((item) => {
+        product.attrs.push(item);
+      });
+    }
+    await product.save();
+
+    res.json({
+      message: "product created",
+      productId: product._id,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const adminUpdateProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id).orFail();
+    const { name, description, count, price, category, attributesTable } =
+      req.body;
+    product.name = name || product.name;
+    product.description = description || product.description;
+    product.count = count || product.count;
+    product.price = price || product.price;
+    product.category = category || product.category;
+    if (attributesTable.length > 0) {
+      product.attrs = [];
+      attributesTable.map((item) => {
+        product.attrs.push(item);
+      });
+    } else {
+      product.attrs = [];
+    }
+    await product.save();
+    res.json({
+      message: "product updated",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const adminUpload = async (req, res, next) => {
+  try {
+    if (!req.files || !!req.files.images === false) {
+      return res.status(400).send("No files were uploaded.");
+    }
+
+    const validateResult = imageValidate(req.files.images);
+    if (validateResult.error) {
+      return res.status(400).send(validateResult.error);
+    }
+
+    const path = require("path");
+    const { v4: uuidv4 } = require("uuid");
+    const uploadDirectory = path.resolve(
+      __dirname,
+      "../../frontend",
+      "public",
+      "images",
+      "products"
+    );
+
+    let product = await Product.findById(req.query.productId).orFail();
+
+    let imagesTable = [];
+    if (Array.isArray(req.files.images)) {
+      imagesTable = req.files.images;
+    } else {
+      imagesTable.push(req.files.images);
+    }
+
+    for (let image of imagesTable) {
+      var fileName = uuidv4() + path.extname(image.name);
+      var uploadPath = uploadDirectory + "/" + fileName;
+      product.images.push({ path: "/images/products/" + fileName });
+      image.mv(uploadPath, function (err) {
+        if (err) {
+          return res.status(500).send(err);
+        }
+      });
+    }
+    await product.save();
+    return res.send("Files uploaded!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+const adminDeleteProductImage = async (req, res, next) => {
+  try {
+    const imagePath = decodeURIComponent(req.params.imagePath);
+    const path = require("path");
+    const finalPath = path.resolve("../frontend/public") + imagePath;
+
+    const fs = require("fs");
+    fs.unlink(finalPath, (err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+    await Product.findOneAndUpdate(
+      { _id: req.params.productId },
+      { $pull: { images: { path: imagePath } } }
+    ).orFail();
+    return res.end();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  getProducts,
+  getProductById,
+  getBestsellers,
+  adminGetProducts,
+  adminDeleteProduct,
+  adminCreateProduct,
+  adminUpdateProduct,
+  adminUpload,
+  adminDeleteProductImage,
+};
 
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,55 @@
+const User = require("../models/UserModel");
+const { hashPassword } = require("../utils/hashPassword");
 
-const getUsers = (req, res) => {
-    res.send("Handling user routes, e.g. get users")
-}
-module.exports = getUsers
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({}).select("-password");
+    return res.json(users);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const registerUser = async (req, res, next) => {
+  try {
+    const { name, lastName, email, password } = req.body;
+    if (!(name && lastName && email && password)) {
+      return res.status(400).send("All inputs are required");
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).send("user exists");
+    } else {
+      const hashedPassword = hashPassword(password);
+      const user = await User.create({
+        name,
+        lastName,
+        email: email.toLowerCase(),
+        password: hashedPassword,
+      });
+      res
+      .cookie("access_token", "fake access token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "strict"
+      })
+        .status(201)
+        .json({
+          success: "User created",
+          userCreated: {
+            _id: user._id,
+            name: user.name,
+            lastName: user.lastName,
+            email: user.email,
+            isAdmin: user.isAdmin,
+          },
+        });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getUsers, registerUser };
+
